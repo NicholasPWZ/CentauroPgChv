64dca05baa7687889a842fbebea0d95e1d11c6da
NicholasPWZ <nicholcosta@hotmail.com>
2023-05-20 17:05:38 -0300

    att

--- Centauro.py
+++ Centauro.py
@@ -2,24 +2,49 @@ from bs4 import BeautifulSoup as bs
 import func
 import service
 
-#Solicitando a url da página chave
-url = input('Insira a página chave da Centauro(#1 é a variável): ')
-#Solicitando a quantidade de páginas a serem adicionadas
-qtd = input('Informe a quantidade de páginas: ')
-qtd = int(qtd) + 1
-url_separada = url.split('#1')
 
-#Adicionando o número das páginas até o numero informado, e após isso inserindo cada tupla presente na lista dentro do banco de dados
-for i in range(qtd):
-    url_formatada = f'{url_separada[0]}{i}{url_separada[1]}'
-    lista_data = func.get_produto(func.pg_chave(url_formatada))
-    for r in lista_data:
-        sku,name,seller,cor,tamanho,preco, url, parcela, vezes = r
-        service.inserir(sku,name,seller,cor,tamanho,preco, url, parcela, vezes)
+#Função para adicionar página chave na Centauro
+def adc_pg_chv():
+    #Solicitando a url da página chave
+    url = input('Insira a página chave da Centauro(#1 é a variável): ')
+    #Solicitando a quantidade de páginas a serem adicionadas
+    qtd = input('Informe a quantidade de páginas: ')
+    qtd = int(qtd) + 1
+    url_separada = url.split('#1')
+
+    #Adicionando o número das páginas até o numero informado, e após isso inserindo cada tupla presente na lista dentro do banco de dados
+    for i in range(qtd):
+        url_formatada = f'{url_separada[0]}{i}{url_separada[1]}'
+        lista_data = func.get_produto(func.pg_chave(url_formatada))
+        for r in lista_data:
+            sku,name,seller,cor,tamanho,preco, url, parcela, vezes = r
+            service.inserir(sku,name,seller,cor,tamanho,preco, url, parcela, vezes)
+
+#Função para procurar um produto através de algum atributo
+def find_by():
+    select = input('Selecione por qual atributo deseja procurar:\n1 - Por SKU\n2 - Por Nome\n3 - Por URL\n4 - Por ID: ')
+    if select == '1':
+        text = input('Informe o SKU a ser procurado: ')
+        service.find_by_sku(text)
+    elif select == '2':
+        text = input('Informe o Nome a ser procurado: ')
+        service.find_by_name(text)
+    elif select == '3':
+        text = input('Informe a URL a ser procurada: ')
+        service.find_by_url(text)
+    elif select == '4':
+        text = input('Informe o ID a ser procurado: ')
+        service.find_by_id(text)
     
-
-
-
+#Menu roda até que o usuário decida quando parar
+while True:
+    action = input('Selecione oq deseja fazer:\n1 - Adicionar pagina chave\n2 - Procurar por um produto especifico\n3 - Finalizar programa: ')
+    if action == '1':
+        adc_pg_chv()
+    elif action == '2':
+        find_by()
+    elif action == '3':
+        break
 
 
 
--- service.py
+++ service.py
@@ -9,3 +9,48 @@ def inserir(sku,name,seller,cor,tamanho, preco, url, parcela, vezes):
                    , (f'{sku}', f'{name}',f'{seller}',f'{cor}',f'{tamanho}', f'{preco}', f'{url}', f'{parcela}', f'{vezes}'))
     conn.commit()
     conn.close()
+
+#Funções de busca para atributos diversos
+def find_by_sku(sku):
+    conn = sqlite3.connect('Centauro_db.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM products where sku LIKE '%{sku}%' ")
+    #Transformando o retorno do select em linhas para retornar em python
+    lines = cursor.fetchall()
+    for i in lines:
+        print(i)
+    conn.commit()
+    conn.close()
+
+def find_by_url(text):
+    conn = sqlite3.connect('Centauro_db.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM products where url LIKE '%{text}%' ")
+    #Transformando o retorno do select em linhas para retornar em python
+    lines = cursor.fetchall()
+    for i in lines:
+        print(i)
+    conn.commit()
+    conn.close()
+
+def find_by_name(text):
+    conn = sqlite3.connect('Centauro_db.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM products where name LIKE '%{text}%' ")
+    #Transformando o retorno do select em linhas para retornar em python
+    lines = cursor.fetchall()
+    for i in lines:
+        print(i)
+    conn.commit()
+    conn.close()
+
+def find_by_id(num):
+    conn = sqlite3.connect('Centauro_db.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM products where id = '{num}' ")
+    #Transformando o retorno do select em linhas para retornar em python
+    lines = cursor.fetchall()
+    for i in lines:
+        print(i)
+    conn.commit()
+    conn.close()
